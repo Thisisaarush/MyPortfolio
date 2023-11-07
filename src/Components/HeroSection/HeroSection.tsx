c7ecb1e62041415a2a26e071abf55ca7cf49e733
Aarush Tanwar <tanwaraarush007@gmail.com>
2023-11-07 13:37:38 +0530

    Update HeroSection.tsx

--- src/Components/HeroSection/HeroSection.tsx
+++ src/Components/HeroSection/HeroSection.tsx
@@ -24,7 +24,7 @@ const HeroSection = () => {
       </span>
       <span className="text-center text-white/70 text-sm z-10 tracking-wide max-w-xl">
         Hi, I'm Aarush, Experienced full stack developer building scalable
-        solutions with Typescript, React(NextJS), Svelte, NodeJS etc.
+        solutions with Typescript, React, NextJS, NodeJS etc.
       </span>
       <Particles className="absolute inset-0" quantity={200} />
     </div>
